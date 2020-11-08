d067a2c127a6d36939635d06f5bebb960c3841c6
IvanJoskic <i.joskic@outlook.com>
2020-11-08 17:34:57 +0100

    Setup and design of simple webpage

--- pet-info/app.js
+++ pet-info/app.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 5000;
 
+const pgp = require('pg-promise')(/* options */);
+const db = pgp('postgres://postgres:bazepodataka@localhost:5432/pets-information');
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.render('index', {
+        title: 'Home',
+    });
+});
+
+app.get('/datatable', async (req, res) => {
+    const petData = await db.one('SELECT * FROM breed LIMIT 1;')
+    .then((data) => {
+        console.log('DATA: ', data);
+        return data;
+    })
+    .catch((error) => {
+        console.log('ERROR: ', error)
+    });
+
+    res.render('datatable', {
+        title: 'Datatable',
+        data: petData,
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About'
+    });
 });
 
 app.listen(port, () => {
-    console.log();
+    console.log(`Listening on port: ${port}`);
 });
